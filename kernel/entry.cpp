a28757365f598a7a7aab198fc491fa924900bad0
KANKOSHEV <31502521+KANKOSHEV@users.noreply.github.com>
2022-02-18 13:18:12 +0300

    Update entry.cpp

--- kernel/entry.cpp
+++ kernel/entry.cpp
@@ -8,29 +8,10 @@ VOID NTAPI entry_thread(PVOID)
 	}
 }
 
-NTSTATUS entry_init()
-{
-	ke_get_current_prcb_address = reinterpret_cast<PVOID>(find_pattern_page_km(
-		"ntoskrnl.exe",
-		".text",
-		"\x65\x48\x8B\x04\x25\x00\x00\x00\x00\xC3\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\x48\x89\x5C\x24\x00",
-		"xxxxx????xxxxxxxxxxxxxxxxxxxxxxxxxxx?"));
-
-	if (ke_get_current_prcb_address == 0)
-		return STATUS_UNSUCCESSFUL;
-
-	return STATUS_SUCCESS;
-}
-
 NTSTATUS entry_point(PVOID, PVOID)
-{
-	NTSTATUS status = STATUS_UNSUCCESSFUL;
-	
-	if (!NT_SUCCESS(entry_init()))
-		return status;
-
+{	
 	HANDLE h_thread = 0;
-	status = PsCreateSystemThread(&h_thread, 0x10000000L, 0, 0, 0, entry_thread, 0);
+	NTSTATUS status = PsCreateSystemThread(&h_thread, 0x10000000L, 0, 0, 0, entry_thread, 0);
 
 	if (!NT_SUCCESS(status))
 		return status;
