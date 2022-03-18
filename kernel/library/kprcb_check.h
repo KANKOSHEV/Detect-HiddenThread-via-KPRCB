262a6e2be1c973c69a90480a9c2be59403790fb1
zach0809 <59408427+zach0809@users.noreply.github.com>
2022-03-18 10:07:37 -0500

    Update kprcb_check.h

--- kernel/library/kprcb_check.h
+++ kernel/library/kprcb_check.h
@@ -20,8 +20,20 @@ void hidden_system_threads_detect()
 			PETHREAD p_thread = 0;
 			PsLookupThreadByThreadId(reinterpret_cast<HANDLE>(current_tid), &p_thread);
 
-			if (p_thread == 0)
+			if (p_thread == 0){
 				DbgPrintEx(0, 0, "[PspCidTable] hidden thread id -> %d\n", current_tid);
+			}else{
+				//Get the thread id of the thread returned.
+				ULONG RetThreadID = reinterpret_cast<ULONG>(PsGetThreadID(p_thread));
+				
+				//Check for swapped PspCidTable entry
+				if(RetThreadID!=current_tid)
+					DbgPrintEx(0, 0, "[PspCidTable] Thread loookup on ID %d returned thread ID %d!\n", current_tid, RetThreadID);
+				
+				//Check for non-system thread returned when the actual thread from KPRCB is a sys thread
+				if(!PsIsSystemThread(p_thread) && PsIsSystemThread((PETHREAD)entry))
+					DbgPrintEx(0, 0, "[PspCidTable] Thread loookup on ID %d returned a non-system thread ID %d!\n", current_tid, RetThreadID);
+			}	
 		}
 	}
 }
